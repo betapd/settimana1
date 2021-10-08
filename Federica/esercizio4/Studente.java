b18d91f43cb431b6c24a8a0b115bfbb5b4eb580d
betapd <92023186+betapd@users.noreply.github.com>
2021-10-08 17:00:36 +0200

    Add files via upload

---
+++ Federica/esercizio4/Studente.java
@@ -0,0 +1,44 @@
+package com.fede.esercizio4;
+
+public class Studente {
+	private int matricola;
+	private String nome;
+	private String cognome;
+	
+	public Studente(int matricola, String nome, String cognome) {
+		this.matricola = matricola;
+		this.nome = nome;
+		this.cognome = cognome;
+	}
+
+	public int getMatricola() {
+		return matricola;
+	}
+
+	public void setMatricola(int matricola) {
+		this.matricola = matricola;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	@Override
+	public String toString() {
+		return "Studente [matricola=" + matricola + ", nome=" + nome + ", cognome=" + cognome + "]";
+	}
+	
+	
+}
