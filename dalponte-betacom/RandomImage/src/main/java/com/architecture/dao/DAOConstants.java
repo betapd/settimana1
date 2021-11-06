0a33884987eac2d998b557a59427282e2861d00d
betapd <92023186+betapd@users.noreply.github.com>
2021-11-06 23:19:24 +0100

    Add files via upload

---
+++ dalponte-betacom/RandomImage/src/main/java/com/architecture/dao/DAOConstants.java
@@ -0,0 +1,11 @@
+package com.architecture.dao;
+
+public interface DAOConstants {
+	String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
+	String JDBC_URL = "jdbc:oracle:thin:@127.0.0.1:1521:orcl";
+	String JDBC_USERNAME = "adalpo";
+	String JDBC_PASSWORD = "pass";
+	
+	String SELECT_IMG= "select * from immagini";
+	
+}
